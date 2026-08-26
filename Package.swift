b07ebb0da22aae52aeddac0416b9eef258ac65fd
Daryle Walker <CTMacUser@users.noreply.github.com>
2026-08-25 22:58:58 -0600

    build: add resource reference
    This was an issue from someone that tried to build this code.
    
    See: <https://www.reddit.com/r/SwiftUI/comments/1v7wzo4/comment/p0my0rf/>.

--- Package.swift
+++ Package.swift
@@ -25,7 +25,10 @@ let package = Package(
     // Targets can depend on other targets in this package and products from
     // dependencies.
     .target(
-      name: "EmailReadingView"
+      name: "EmailReadingView",
+      resources: [
+        .process("Localizable.xcstrings"),
+      ]
     ),
     .testTarget(
       name: "EmailReadingViewTests",
